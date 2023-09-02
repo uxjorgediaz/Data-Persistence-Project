520bcd0918675cbf80b25149d20ac7c9b344756b
Jorge Diaz <110213195+uxjorgediaz@users.noreply.github.com>
2023-09-02 10:48:38 -0500

    UI button menu complete
    All the UI button menu are assigned

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+}
